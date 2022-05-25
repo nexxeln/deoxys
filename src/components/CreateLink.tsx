89856714ca968291e66d58d349e7340a347c05ab
Shoubhit Dash <shoubhit2005@gmail.com>
2022-05-25 17:55:57 +0530

    convert input to lowercase

--- src/components/CreateLink.tsx
+++ src/components/CreateLink.tsx
@@ -77,7 +77,7 @@ const CreateLink = () => {
           onChange={(e) => {
             setForm({
               ...form,
-              slug: e.target.value,
+              slug: e.target.value.toLowerCase(),
             });
 
             debounce(checkSlug.refetch, 100);
@@ -117,7 +117,9 @@ const CreateLink = () => {
           type="url"
           value={form.url}
           maxLength={3000}
-          onChange={(e) => setForm({ ...form, url: e.target.value })}
+          onChange={(e) =>
+            setForm({ ...form, url: e.target.value.toLowerCase() })
+          }
           placeholder="https://google.com"
           className="block w-full px-4 py-2 font-normal bg-black border-2 border-gray-200 rounded-md focus:outline-none placeholder:text-gray-400"
           required
