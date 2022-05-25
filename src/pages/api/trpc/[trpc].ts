739b20009c0548640f62db95c50bc3d906f5bdc6
Shoubhit Dash <shoubhit2005@gmail.com>
2022-05-25 14:47:37 +0530

    create mutation to create short link

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -4,20 +4,37 @@ import { z } from "zod";
 
 import { prisma } from "../../../db/client";
 
-export const appRouter = trpc.router().query("checkSlug", {
-  input: z.object({ slug: z.string() }),
-  async resolve({ input }) {
-    const slugCount = await prisma.shortLink.count({
-      where: {
-        slug: {
-          equals: input.slug,
+export const appRouter = trpc
+  .router()
+  .query("checkSlug", {
+    input: z.object({ slug: z.string() }),
+    async resolve({ input }) {
+      const slugCount = await prisma.shortLink.count({
+        where: {
+          slug: {
+            equals: input.slug,
+          },
         },
-      },
-    });
+      });
 
-    return { used: slugCount > 0 };
-  },
-});
+      return { used: slugCount > 0 };
+    },
+  })
+  .mutation("createShortLink", {
+    input: z.object({ slug: z.string(), url: z.string() }),
+    async resolve({ input }) {
+      try {
+        await prisma.shortLink.create({
+          data: {
+            slug: input.slug,
+            url: input.url,
+          },
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  });
 
 export type AppRouter = typeof appRouter;
 
