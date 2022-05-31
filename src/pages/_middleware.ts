a2cd01d9d2a25632c8a70bc641ac4545b0f87b94
Shoubhit Dash <shoubhit2005@gmail.com>
2022-05-31 23:56:30 +0530

    improve readability

--- src/pages/_middleware.ts
+++ src/pages/_middleware.ts
@@ -1,6 +1,6 @@
 import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
 
-export async function middleware(req: NextRequest, ev: NextFetchEvent) {
+export async function middleware(req: NextRequest, event: NextFetchEvent) {
   if (
     req.nextUrl.pathname.startsWith("/api/") ||
     req.nextUrl.pathname === "/"
